aac1523b0b837830028c72f24bbf506aa6909e16
rikumakkonen <riku.makkonen@gmail.com>
2012-07-06 20:51:03 +0300

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orxtersongs::Application.config.secret_token = '90625861cec16e175f23c9cdb710e51dc1e6379b8b4e2b59ce3139be1b02f74343820b8ea33f33967f7e1bc59ffe054df166e237843345b4f417380e1457de92'
